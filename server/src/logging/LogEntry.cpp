2870a412ff9d1477daf335f729fda12555c102a7
PerekatitmoPole <gazizov.egor2017@yandex.ru>
2026-05-03 14:30:34 +0300

    Fix CI

--- server/src/logging/LogEntry.cpp
+++ server/src/logging/LogEntry.cpp
@@ -4,6 +4,10 @@
 #include <iomanip>
 #include <sstream>
 
+extern "C" {
+#include <time.h>
+}
+
 namespace currency::logging {
 
 std::string toString(const LogLevel level) {
@@ -22,7 +26,15 @@ std::string toString(const LogLevel level) {
 std::string toIsoUtc(const std::chrono::system_clock::time_point timestamp) {
     const auto time = std::chrono::system_clock::to_time_t(timestamp);
     std::tm utcTime{};
-    gmtime_s(&utcTime, &time);
+#if defined(_WIN32) && !defined(__MINGW32__)
+    if (gmtime_s(&utcTime, &time) != 0) {
+        return {};
+    }
+#else
+    if (gmtime_r(&time, &utcTime) == nullptr) {
+        return {};
+    }
+#endif
 
     std::ostringstream stream;
     stream << std::put_time(&utcTime, "%Y-%m-%dT%H:%M:%SZ");
